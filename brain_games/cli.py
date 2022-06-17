cef3785cf7da56754cdb2081a8d8d988172b3b00
Gennadii Gorshkov <gorshkovgena@gmail.com>
2022-06-17 18:14:03 +0300

    fix files after linter check

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,6 +1,9 @@
+"""Empty docstring."""
+
 import prompt
 
-def welcome_user():
-    name = prompt.string('May I have your name? ')
-    print(f"Hello, {name}!")
 
+def welcome_user():
+    """Return welcome message with username."""
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
