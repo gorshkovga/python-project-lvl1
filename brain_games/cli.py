005c48df6ef59b631c555f48c135501e4a98ffcb
Gennadii Gorshkov <gorshkovgena@gmail.com>
2022-08-17 20:04:37 +0300

    fix all *.py after linter

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,5 +1,8 @@
+"""Module with greeting function."""
 import prompt
 
+
 def welcome_user():
+    """Ask username and print greeting."""
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
+"""Main module."""
+
 from brain_games.cli import welcome_user
 
+
 def main():
+    """Call functions from other modules."""
     print('Welcome to the Brain Games!')
     welcome_user()
+
 
 if __name__ == '__main__':
     main()
