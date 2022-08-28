52bc0a36da9c06da4f362a9544657a8b6c513e55
Gennadii Gorshkov <gorshkovgena@gmail.com>
2022-08-28 12:46:57 +0300

    update game_calc.py (new version of game_calc.py

--- brain_games/game_calc.py
+++ brain_games/game_calc.py
@@ -11,17 +11,9 @@ def game(user_name):
     Parameters:
     user_name -- user name
     """
-    print('What is the result of the expression?')
-    counter = 0
-    while counter < 3:
-        equation = make_equation()
-        print(f'Question: {equation}')
-        answer = string('Your answer: ')
-        true_answer = str(int(eval(equation)))
-        check_result = check_answer(user_name, answer, true_answer)
-        if check_result:
-            counter += 1
-            if counter == 3:
-                print(f'Congratulations, {user_name}!')
-        elif not check_result:
-            break
+    equation = make_equation()
+    print(f'Question: {equation}')
+    answer = string('Your answer: ')
+    true_answer = str(int(eval(equation)))
+    check_result = check_answer(user_name, answer, true_answer)
+    return check_result
