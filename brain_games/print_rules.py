68e936a12bf1cec06b98ba5858b7105c6b50a98c
Gennadii Gorshkov <gorshkovgena@gmail.com>
2022-08-28 12:44:36 +0300

    add print_rules.py

---
+++ brain_games/print_rules.py
@@ -0,0 +1,11 @@
+"""Module with game rules."""
+
+
+def print_rules(game_name):
+    """Prrint game rules."""
+    if game_name == 'even':
+        print('Answer "yes" if the number is even, otherwise answer "no".')
+    elif game_name == 'calc':
+        print('What is the result of the expression?')
+    elif game_name == 'gcd':
+        print('Find the greatest common divisor of given numbers.')
