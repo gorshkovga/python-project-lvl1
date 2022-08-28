2a20d4d1c020fcae0794e0e3fcbdd5cedd0c350b
Gennadii Gorshkov <gorshkovgena@gmail.com>
2022-08-28 12:46:24 +0300

    update game_even.py (new version of game_even.py

--- brain_games/game_even.py
+++ brain_games/game_even.py
@@ -13,17 +13,9 @@ def game(user_name):
     Parameters:
     user_name -- user name
     """
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    counter = 0
-    while counter < 3:
-        number = randint(0, 100)  # noqa: S311
-        print(f'Question: {number}')
-        answer = string('Your answer: ')
-        true_answer = is_even(number)
-        check_result = check_answer(user_name, answer, true_answer)
-        if check_result:
-            counter += 1
-            if counter == 3:
-                print(f'Congratulations, {user_name}!')
-        elif not check_result:
-            break
+    number = randint(0, 100)  # noqa: S311
+    print(f'Question: {number}')
+    answer = string('Your answer: ')
+    true_answer = is_even(number)
+    check_result = check_answer(user_name, answer, true_answer)
+    return check_result
