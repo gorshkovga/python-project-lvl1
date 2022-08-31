85b660624d859fde65a4d41856884fce75bb071a
Gennadii Gorshkov <gorshkovgena@gmail.com>
2022-08-31 21:22:11 +0300

    add game_prime.py

---
+++ brain_games/game_prime.py
@@ -0,0 +1,22 @@
+"""Game module."""
+
+from random import randint
+
+from brain_games.check_answer import check_answer
+from brain_games.is_prime import is_prime
+from prompt import string
+
+
+def game(user_name):
+    """Do game.
+
+    Parameters:
+    user_name -- user name
+    Returns:
+    return logical result of level passing
+    """
+    number = randint(0, 100)  # noqa: S311
+    print(f'Question: {number}')
+    answer = string('Your answer: ')
+    true_answer = is_prime(number)
+    return check_answer(user_name, answer, true_answer)
