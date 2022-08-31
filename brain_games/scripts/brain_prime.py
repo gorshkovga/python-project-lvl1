426034fde58487db8dbdf2bbd173aeaeed912ecf
Gennadii Gorshkov <gorshkovgena@gmail.com>
2022-08-31 21:22:33 +0300

    add brain_prime.py

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Main module."""
+
+from brain_games.game_prime import game
+from brain_games.greeting import great_user
+from brain_games.make_game import make_game
+from brain_games.print_rules import print_rules
+
+
+def main():
+    """Call functions from other modules."""
+    user_name = great_user()
+    print_rules('prime')
+    make_game(game, user_name)
+
+
+if __name__ == '__main__':
+    main()
