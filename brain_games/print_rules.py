d8f458216edfa60537c86dffd82bf44d95fbe532
Gennadii Gorshkov <gorshkovgena@gmail.com>
2022-08-31 21:24:20 +0300

    fix print_rules.py

--- brain_games/print_rules.py
+++ brain_games/print_rules.py
@@ -1,9 +1,9 @@
 """Module with games rules."""
 
 
-def print_rules(game_name):
+def print_rules(game_name):  # noqa: WPS231
     """Prrint game rules."""
-    if game_name == 'even':
+    if game_name == 'even':  # noqa: WPS223
         print('Answer "yes" if the number is even, otherwise answer "no".')
     elif game_name == 'calc':
         print('What is the result of the expression?')
