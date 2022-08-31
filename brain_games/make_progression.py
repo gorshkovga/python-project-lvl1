4d5b85d4226412198eecedd279c1aa7f104ddef8
Gennadii Gorshkov <gorshkovgena@gmail.com>
2022-08-31 19:56:21 +0300

    add make_progression.py

---
+++ brain_games/make_progression.py
@@ -0,0 +1,26 @@
+"""Progression generator module."""
+
+from random import randint
+
+
+def make_progression():  # noqa: WPS210
+    """Make random arithmetic progression.
+
+    Returns:
+    return hidden progression member
+    """
+    start = randint(0, 100)  # noqa: S311, WPS432
+    step = randint(1, 10)  # noqa: S311, WPS432
+    length = randint(5, 15)  # noqa: S311, WPS432
+    hidden_member_index = randint(1, length)  # noqa: S311, WP432
+    ind = 1
+    progression = ''
+    while ind <= length:
+        if ind == hidden_member_index:
+            progression = f'{progression} ..'
+            hidden_member = str(start + (ind - 1) * step)
+        else:
+            member = str(start + (ind - 1) * step)
+            progression = f'{progression} {member}'
+        ind += 1
+    return (hidden_member, progression.strip())
