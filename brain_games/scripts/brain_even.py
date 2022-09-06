7624eb8c741c92cf1732bbec1ea242d723841562
Gennadii Gorshkov <gorshkovgena@gmail.com>
2022-09-06 15:19:29 +0300

    update brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python3
 """Main module."""
 
-from brain_games.game_even import game
-from brain_games.greeting import great_user
-from brain_games.make_game import make_game
-from brain_games.print_rules import print_rules
+from games.game_even import game, RULES
+from games.make_game import make_game
 
 
 def main():
     """Call functions from other modules."""
-    user_name = great_user()
-    print_rules('even')
-    make_game(game, user_name)
+    make_game(game, RULES)
 
 
 if __name__ == '__main__':
