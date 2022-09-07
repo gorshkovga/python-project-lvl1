1e40f8eb5292427c4a5d043ef3abdffe48a13e86
Gennadii Gorshkov <gorshkovgena@gmail.com>
2022-09-07 18:15:08 +0300

    update game_calc.py

--- brain_games/games/game_calc.py
+++ brain_games/games/game_calc.py
@@ -5,6 +5,7 @@ from random import choice, randint
 
 RULES = 'What is the result of the expression?'
 
+
 def game():
     """Do game and make question.
 
@@ -14,6 +15,8 @@ def game():
     number_one = randint(0, 100)  # noqa: S311
     number_two = randint(0, 100)  # noqa: S311
     operations = (' + ', ' - ', ' * ')
-    equation = str(number_one) + choice(operations) + str(number_two)  # noqa: S311
+    equation = (
+        str(number_one) + choice(operations) + str(number_two)  # noqa: S311
+    )
     print(f'Question: {equation}')
     return str(int(eval(equation)))  # noqa: S307
