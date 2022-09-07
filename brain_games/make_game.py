45c511c368c6ee575017dd92531095e6010f04f3
Gennadii Gorshkov <gorshkovgena@gmail.com>
2022-09-07 13:26:55 +0300

    move make_game.py

--- brain_games/make_game.py
+++ brain_games/make_game.py
@@ -1,19 +1,35 @@
 """Game making module."""
+from prompt import string
 
 
-def make_game(game_function, user_name):
+def make_game(game_function, rules):  # noqa: WPS210
     """Do game levels iterations.
 
     Parameters:
     game_function -- function with game logics
-    user_name -- user name
+    rules -- game rules
     """
+    print('Welcome to the Brain Games!')
+    name = string('May I have your name? ')
+    print(f'Hello, {name}!')
+    print(rules)
+
     counter = 0
     while counter < 3:
-        level_result = game_function(user_name)
+        true_answer = game_function()
+        user_answer = string('Your answer: ')
+        level_result = (user_answer == true_answer)
         if level_result:
+            print('Correct!')
             counter += 1
             if counter == 3:
-                print(f'Congratulations, {user_name}!')
+                print(f'Congratulations, {name}!')
         elif not level_result:
+            long_message = (
+                f"'{user_answer}' is wrong answer ;(. "
+                +  # noqa: W503, W504
+                f"Correct answer was '{true_answer}'."
+            )
+            print(long_message)
+            print(f"Let's try again, {name}!")
             break
