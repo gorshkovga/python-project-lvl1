a4ae8b3534b60bbfca084bee9895cd40312af5d3
Gennadii Gorshkov <gorshkovgena@gmail.com>
2022-09-08 17:50:42 +0300

    update game_calc.py

--- brain_games/games/game_calc.py
+++ brain_games/games/game_calc.py
@@ -10,7 +10,7 @@ def game():
     """Do game and make question.
 
     Returns:
-    return true answer
+    return question, true answer
     """
     number_one = randint(0, 100)  # noqa: S311
     number_two = randint(0, 100)  # noqa: S311
@@ -18,5 +18,4 @@ def game():
     equation = (
         str(number_one) + choice(operations) + str(number_two)  # noqa: S311
     )
-    print(f'Question: {equation}')
-    return str(int(eval(equation)))  # noqa: S307
+    return equation, str(int(eval(equation)))  # noqa: S307
