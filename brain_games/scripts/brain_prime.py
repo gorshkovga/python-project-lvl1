0682becf3d240744242a3f20af177c397f774543
Gennadii Gorshkov <gorshkovgena@gmail.com>
2022-09-08 18:09:45 +0300

    update brain_prime.py

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """Main module."""
 
-from brain_games.games.game_prime import RULES, game
+import brain_games.games.game_prime as game_module
 from brain_games.make_game import make_game
 
 
 def main():
-    """Call functions from other modules."""
-    make_game(game, RULES)
+    """Call functions from other module."""
+    make_game(game_module)
 
 
 if __name__ == '__main__':
