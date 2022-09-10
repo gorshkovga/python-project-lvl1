8fca61541896da783368ecc14ad9f3798a1e5dad
Gennadii Gorshkov <gorshkovgena@gmail.com>
2022-09-10 15:13:46 +0300

    update calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -15,7 +15,14 @@ def make_question():
     number_one = randint(0, 100)
     number_two = randint(0, 100)
     operations = (' + ', ' - ', ' * ')
+    operation = choice(operations)
     equation = (
-        str(number_one) + choice(operations) + str(number_two)
+        str(number_one) + operation + str(number_two)
     )
-    return equation, str(int(eval(equation)))
+    if operation == ' + ':
+        result = number_one + number_two
+    elif operation == ' - ':
+        result = number_one - number_two
+    else:
+        result = number_one * number_two
+    return equation, str(result)
