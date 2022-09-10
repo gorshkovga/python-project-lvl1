0d74e155ea17e025a4e9e536756e4c9e14a65613
Gennadii Gorshkov <gorshkovgena@gmail.com>
2022-09-10 13:46:44 +0300

    update run_game.py

--- brain_games/run_game.py
+++ brain_games/run_game.py
@@ -12,21 +12,18 @@ def run_game(game, number_of_levels=3):
     name = string('May I have your name? ')
     print(f'Hello, {name}!')
     print(game.RULES)
-    counter = 0
-    while counter < number_of_levels:
+    for level in range(0, number_of_levels):
         question, true_answer = game.game()
         print(f'Question: {question}')
         user_answer = string('Your answer: ')
-        level_result = (user_answer == true_answer)
-        if level_result:
+        if user_answer == true_answer:
             print('Correct!')
-            counter += 1
-            if counter == 3:
-                print(f'Congratulations, {name}!')
-        elif not level_result:
+        else:
             print(
                 f"'{user_answer}' is wrong answer ;(. "
                 + f"Correct answer was '{true_answer}'."  # noqa: W503
             )
             print(f"Let's try again, {name}!")
             break
+    else:
+        print(f'Congratulations, {name}!')
