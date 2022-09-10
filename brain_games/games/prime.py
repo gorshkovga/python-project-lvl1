6cbf02669ebd783e8beea136ec8cd6e9c2c63624
Gennadii Gorshkov <gorshkovgena@gmail.com>
2022-09-10 15:26:52 +0300

    update prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -5,6 +5,20 @@ from random import randint
 RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
+def is_prime(number):
+    """Check is number is prime or not.
+    Arguments:
+    number -- nuber for checking
+    Returns:
+    return logical answer
+    """
+    ind = 2
+    while ind < number:
+        if number % ind == 0:
+            return False
+        ind += 1
+    return True if number > 1 else False
+
 def make_question():
     """Make question and answer.
 
@@ -12,9 +26,5 @@ def make_question():
     return question, true answer
     """
     number = randint(0, 100)
-    ind = 2
-    while ind < number:
-        if number % ind == 0:
-            return number, 'no'
-        ind += 1
-    return number, 'yes' if number > 1 else 'no'
+    result = is_prime(number)
+    return number, 'yes' if result else 'no'
